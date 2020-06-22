9d628f9f69a80cb95c34fe22c812ac73d3e4f9d1
Juliane Pires <mpiresjuliane@outlook.com>
2020-06-21 23:56:57 -0300

    Commit inicial

---
+++ pluginProdSC/index.live.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<img src="icon-sc.png">
+	<title>Scriptcase Prod</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+	<script src="./js/funcoes.js" type="text/javascript"></script>
+</head>
+<body>
+
+
+</body>
+</html>
